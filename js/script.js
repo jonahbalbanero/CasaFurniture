f9c662ad08df8d09be9a9f49e5daeed46dd9bb9b
jonahbalbanero <150066677+jonahbalbanero@users.noreply.github.com>
2023-11-28 23:48:45 -0500

    main

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 // script.js
 
-const modelViewerColor = document.querySelector("model-viewer#color");
-
 // 1. Explore Different Interactions with Hotspots
 document.querySelectorAll('.Hotspot').forEach((hotspot, index) => {
     hotspot.addEventListener('click', () => {
@@ -11,29 +9,13 @@ document.querySelectorAll('.Hotspot').forEach((hotspot, index) => {
 });
 
 // 2. Explore Variations by Swapping Colors
-document.querySelectorAll('.controls button').forEach(button => {
-    button.addEventListener('click', () => {
-        const colorString = button.dataset.color;
-        const material = modelViewerColor.model.materials[0].pbrMetallicRoughness.baseColorFactor;
-        material[0] = hexToRgb(colorString).r / 255;
-        material[1] = hexToRgb(colorString).g / 255;
-        material[2] = hexToRgb(colorString).b / 255;
+const modelViewerColor = document.querySelector("model-viewer#color");
 
-        // Ensure that the model updates
-        modelViewerColor.model.materials[0].pbrMetallicRoughness.baseColorFactor = material;
-        modelViewerColor.dispatchEvent(new CustomEvent('load'));
-    });
+document.querySelector('#color-controls').addEventListener('click', (event) => {
+  const colorString = event.target.dataset.color;
+  const [material] = modelViewerColor.model.materials;
+  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
-
-function hexToRgb(hex) {
-    // Convert a hex color code to an RGB object
-    const bigint = parseInt(hex.slice(1), 16);
-    return {
-        r: (bigint >> 16) & 255,
-        g: (bigint >> 8) & 255,
-        b: bigint & 255
-    };
-}
 
 // 3. Use Camera Views to Showcase Specific Aspects
 document.querySelector('#showcase-view').addEventListener('click', () => {
