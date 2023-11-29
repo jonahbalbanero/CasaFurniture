815fbc78462d23a820d5b09600e53baac8f0880a
jonahbalbanero <150066677+jonahbalbanero@users.noreply.github.com>
2023-11-29 00:21:28 -0500

    main

--- js/script.js
+++ js/script.js
@@ -9,25 +9,52 @@ document.querySelectorAll('.Hotspot').forEach((hotspot, index) => {
 });
 
 // 2. Explore Variations by Swapping Colors
-const modelViewerColor = document.querySelector("model-viewer#color");
+const chairModelViewer = document.querySelector("#chair-model-viewer");
+const sofaModelViewer = document.querySelector("#sofa-model-viewer");
+const lampModelViewer = document.querySelector("#lamp-model-viewer");
 
-document.querySelector('#color-controls').addEventListener('click', (event) => {
-  const colorString = event.target.dataset.color;
-  const [material] = modelViewerColor.model.materials;
-  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+document.querySelector('#chair-color-controls').addEventListener('click', (event) => {
+    updateColor(chairModelViewer, event);
 });
+
+document.querySelector('#sofa-color-controls').addEventListener('click', (event) => {
+    updateColor(sofaModelViewer, event);
+});
+
+document.querySelector('#lamp-color-controls').addEventListener('click', (event) => {
+    updateColor(lampModelViewer, event);
+});
+
+function updateColor(modelViewer, event) {
+    const colorString = event.target.dataset.color;
+    const material = modelViewer.model.materials[0].pbrMetallicRoughness;
+    material.setBaseColorFactor(colorString);
+
+    // Ensure that the model updates
+    modelViewer.dispatchEvent(new CustomEvent('load'));
+}
 
 // 3. Use Camera Views to Showcase Specific Aspects
 document.querySelector('#showcase-view').addEventListener('click', () => {
     // Change the camera position to showcase a specific aspect of the product
-    modelViewerColor.cameraOrbit = '0deg 90deg 2m';
-    modelViewerColor.cameraTarget = '0m 0.5m 0m';
+    chairModelViewer.cameraOrbit = '0deg 90deg 2m';
+    chairModelViewer.cameraTarget = '0m 0.5m 0m';
+
+    sofaModelViewer.cameraOrbit = '0deg 90deg 2m';
+    sofaModelViewer.cameraTarget = '0m 0.5m 0m';
+
+    lampModelViewer.cameraOrbit = '0deg 90deg 2m';
+    lampModelViewer.cameraTarget = '0m 0.5m 0m';
 });
 
 document.querySelector('#reset-view').addEventListener('click', () => {
     // Reset the camera position to the default
-    modelViewerColor.cameraOrbit = '0deg 90deg 2m';
-    modelViewerColor.cameraTarget = '0m 0.5m 0m';
+    chairModelViewer.cameraOrbit = '0deg 90deg 2m';
+    chairModelViewer.cameraTarget = '0m 0.5m 0m';
+
+    sofaModelViewer.cameraOrbit = '0deg 90deg 2m';
+    sofaModelViewer.cameraTarget = '0m 0.5m 0m';
+
+    lampModelViewer.cameraOrbit = '0deg 90deg 2m';
+    lampModelViewer.cameraTarget = '0m 0.5m 0m';
 });
-
-
